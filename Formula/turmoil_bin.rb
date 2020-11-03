34e98bb1a8814c57644261393e3591e38b2a186f
Peter Bjorklund <piot@hotmail.com>
2020-11-03 14:00:42 +0100

    chore: remove unneeded

--- Formula/turmoil_bin.rb
+++ Formula/turmoil_bin.rb
@@ -5,7 +5,7 @@ class TurmoilBin < Formula
   url "https://github.com/piot/turmoil-dist/releases/download/0.0.0/turmoil.0.0.8.tar.bz2"
   sha256 "d23f0f4225f6e398469502d877d19bf186c29db8a442d2207d993ca76b62b8a1"
 
-  bottle :unneeded
+ # bottle :unneeded
 
   def install
     bin.install "bin/swamp"
