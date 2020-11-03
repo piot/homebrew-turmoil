e2ce5e1c183fe00db711408b143c70fcd06f9e31
Peter Bjorklund <piot@hotmail.com>
2020-11-03 13:15:38 +0100

    feat: release 0.0.8

--- Formula/turmoil_bin.rb
+++ Formula/turmoil_bin.rb
@@ -2,16 +2,20 @@ class TurmoilBin < Formula
   desc "2D Engine"
   homepage "https://spelmotor.com"
 
-  url "https://github.com/piot/turmoil-dist/releases/download/0.0.0/turmoil.0.0.6.tar.gz"
-  sha256 "bcd778a64605b6645ccb21be5823189a58e15799ecd0624e95c9c29d23ffa595"
+  url "https://github.com/piot/turmoil-dist/releases/download/0.0.0/turmoil.0.0.8.tar.bz2"
+  sha256 "2398819a6c6ae26424ad0360409b9af93be92b401a764957b2f8b14943b0ed49"
 
   bottle :unneeded
 
   def install
     bin.install "bin/swamp"
+    bin.install "bin/turmoilc"
+    bin.install "bin/turmoil_linux" => "turmoil" if OS.linux?
+    bin.install "bin/turmoil_mac" => "turmoil" if OS.mac?
   end
 
   test do
     system "#{bin}/swamp", "version"
+    system "#{bin}/turmoil", "version"
   end
 end
