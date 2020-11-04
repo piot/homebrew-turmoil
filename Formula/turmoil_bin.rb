a90dc699299530d5d7cb440d867d1728fe7f7dfb
Peter Bjorklund <piot@hotmail.com>
2020-11-04 16:30:01 +0100

    fix: os specific extensions

--- Formula/turmoil_bin.rb
+++ Formula/turmoil_bin.rb
@@ -2,16 +2,21 @@ class TurmoilBin < Formula
   desc "2D Engine"
   homepage "https://spelmotor.com"
 
-  url "https://github.com/piot/turmoil-dist/releases/download/0.0.0/turmoil.0.0.8.tar.bz2"
-  sha256 "d23f0f4225f6e398469502d877d19bf186c29db8a442d2207d993ca76b62b8a1"
+  url "https://github.com/piot/turmoil-dist/releases/download/0.0.0/turmoil.0.0.9.tar.bz2"
+  sha256 "14eb51c7886b285bbdeb67753412bf7aa2eca7a2840c651753df5e4dd44c1cb6"
 
  # bottle :unneeded
 
   def install
-    bin.install "bin/swamp"
+    os_extension = ""
+    if OS.linux?
+      os_extension = "linux_amd64"
+    elsif OS.mac?
+      os_extension = "darwin_amd64"
+    end
+    bin.install "bin/turmoil_#{os_extension}" => "turmoil"
+    bin.install "bin/swamp_#{os_extension}" => "swamp"
     bin.install "bin/turmoilc"
-    bin.install "bin/turmoil_linux" => "turmoil" if OS.linux?
-    bin.install "bin/turmoil_mac" => "turmoil" if OS.mac?
 
     opt_prefix.install Dir["packages/"]
   end
