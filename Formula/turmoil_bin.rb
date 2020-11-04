5b17909dad27e50a68cafc766d70fb0ff6a9a009
Peter Bjorklund <piot@hotmail.com>
2020-11-04 18:08:57 +0100

    chore: update sha256

--- Formula/turmoil_bin.rb
+++ Formula/turmoil_bin.rb
@@ -3,16 +3,15 @@ class TurmoilBin < Formula
   homepage "https://spelmotor.com"
 
   url "https://github.com/piot/turmoil-dist/releases/download/0.0.0/turmoil.0.0.9.tar.bz2"
-  sha256 "14eb51c7886b285bbdeb67753412bf7aa2eca7a2840c651753df5e4dd44c1cb6"
+  sha256 "f63b50fd66a3cf32564ac09377c39ae203307ddeb4111b759ed257b6fe68ca9d"
 
   bottle :unneeded
 
   def install
-    os_extension = ""
+    os_extension = "darwin_amd64"
     if OS.linux?
       os_extension = "linux_amd64"
     elsif OS.mac?
-      os_extension = "darwin_amd64"
     end
     bin.install "bin/turmoil_#{os_extension}" => "turmoil"
     bin.install "bin/swamp_#{os_extension}" => "swamp"
